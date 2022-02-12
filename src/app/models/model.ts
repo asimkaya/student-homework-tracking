e11715d58da744b263801be1df46ac643a45e57c
Asım Emre Kaya <asim@montent.com>
2022-02-12 18:04:03 +0300

    models created

---
+++ src/app/models/model.ts
@@ -0,0 +1,37 @@
+export interface Users {
+    id: number;
+    firstName: string;
+    lastName: string;
+    username: string;
+    password: string;
+    userType: number;
+    classId: number;
+    createdOn: Date;
+    status: number;
+}
+
+export interface HomeWork {
+    id: number;
+    name: string;
+    courseId: number;
+    classId: number;
+    description?: string;
+    homeWorkStatus: string;
+    createdOn: Date;
+    status: string;
+}
+
+export interface Classes {
+    id: number;
+    name: string;
+    description?: string;
+    createdOn: Date;
+    status: string;
+}
+
+export interface Courses {
+    id: number;
+    name: string;
+    createdOn: Date;
+    status: number;
+}
